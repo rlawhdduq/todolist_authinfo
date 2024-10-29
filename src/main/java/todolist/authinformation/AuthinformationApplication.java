8515c60a2bce063658f51eb93c10f571be8ecee0
kjy <rlawhdduq95@gmail.com>
2024-10-29 20:20:05 +0900

    최초 업로드

---
+++ src/main/java/todolist/authinformation/AuthinformationApplication.java
@@ -0,0 +1,13 @@
+package todolist.authinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthinformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthinformationApplication.class, args);
+	}
+
+}
