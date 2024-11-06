701c7fc2a2b46042b606338c537f3ddecb9f4ad8
kjy <rlawhdduq95@gmail.com>
2024-11-06 19:52:26 +0900

    사용자 인증 토큰 생성부

---
+++ src/main/java/todolist/authinformation/dto/AuthInfoDto.java
@@ -0,0 +1,15 @@
+package todolist.authinformation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthInfoDto {
+    private Long user_id;
+    private String id;
+    private String user_type;
+    private String scope_of_disclosure;
+}
---
+++ src/test/java/todolist/authinformation/api/AuthApiTest.java
@@ -0,0 +1,47 @@
+package todolist.authinformation.api;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import todolist.authinformation.dto.AuthInfoDto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthApiTest {
+    private static final Logger log = LoggerFactory.getLogger(AuthApiTest.class);
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testCreateToken() 
+    throws Exception
+    {
+        log.info("token CreateStart");
+        String createTokenRequest = objectMapper.writeValueAsString(new AuthInfoDto(46L, "test1238", "NC", "A"));
+
+        ResultActions resultActions = mockMvc.perform(post("/api/token")
+                                            .contentType(MediaType.APPLICATION_JSON)
+                                            .content(createTokenRequest))
+                                            .andExpect(status().isOk());
+        log.info("return json : " + resultActions.andReturn().getResponse());
+        log.info("token CreateEnd");
+    }
+    
+}
