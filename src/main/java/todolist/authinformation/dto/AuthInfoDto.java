142f81e1265f218d4c61b108e57ac6131bdc13d4
kjy <rlawhdduq95@gmail.com>
2025-11-10 00:48:26 +0900

    @ToString 추가(로그찍기용)

--- src/main/java/todolist/authinformation/dto/AuthInfoDto.java
+++ src/main/java/todolist/authinformation/dto/AuthInfoDto.java
@@ -3,10 +3,12 @@ package todolist.authinformation.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class AuthInfoDto {
     private Long user_id;
     private String id;
